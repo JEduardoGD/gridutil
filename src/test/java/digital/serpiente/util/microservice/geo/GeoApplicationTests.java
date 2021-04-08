21d85699ab1ec65c1967fb5863b878e72e018cad
Eduardo Gutierrez <eduardo_gd@hotmail.com>
2021-04-08 16:19:00 -0500

    first commit

---
+++ src/test/java/digital/serpiente/util/microservice/geo/GeoApplicationTests.java
@@ -0,0 +1,13 @@
+package digital.serpiente.util.microservice.geo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
